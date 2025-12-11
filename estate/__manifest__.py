ee3229e6b46ca2051a3a4c64ff31a9244ecd6b3b
ARHANOID <realkrik@gmail.com>
2025-12-11 21:36:15 +0200

    changed names for units and profile modules

---
+++ estate/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+    'summary': """
+        Starting module for "Master the Odoo web framework, chapter 0: Create a estate View"
+    """,
+
+    'description': """
+        Starting module for "Master the Odoo web framework, chapter 0: Create a estate View"
+    """,
+
+    'version': '0.1',
+    'application': True,
+    'category': 'Tutorials/estate',
+    'installable': True,
+    'depends': ['web', 'base'],
+    'data': [
+
+    ],
+    'assets': {
+        
+    },
+    'license': 'AGPL-3'
+}
---
+++ estate/models/test_model.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+class TestModel(models.Model):
+    _name = "test_model"
+    _description = "Test test_model Model"
+
+    name = fields.Char(required=True, default="Unknown0")
+    action = fields.Char(required=True, default="Unknown1")
+    action1 = fields.Char(required=True, default="Unknown2")
+    action2 = fields.Char(required=True, default="Unknown3")
+    action3 = fields.Char(required=True, default="Unknown4")
+    action4 = fields.Char(required=True, default="Unknown5")
+    action5 = fields.Char(required=True, default="Unknown6")
+    action6 = fields.Char(required=True, default="Unknown7")
+    action7 = fields.Char(required=True, default="Unknown8")
+    action8 = fields.Char(required=True, default="Unknown9")
+    action9 = fields.Char(required=True, default="Unknown99")
